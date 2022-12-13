14483b3f5b3dc4d0e4fa944e58e4b892f08ad6af
charan250498 <charan2598@gmail.com>
2022-12-13 15:30:30 -0800

    Initial commit with player and enemy characters.

---
+++ VRGame/Assets/Scripts/enemy.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject[] targets;
+    public GameObject player;
+
+    // Enemy gun end and start points
+    public GameObject end, start;
+
+    float gunShotTime = 0.1f;
+
+    int target_index = 0;
+    int patrolling = 1;
+    float detection_angle = 30.0f;
+    float lethal_distance = 4.0f;
+    float detection_distance = 12.0f;
+
+    public float health = 100.0f;
+    public bool isDead = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //animator.SetTrigger("die");
+        transform.rotation = Quaternion.LookRotation(targets[0].transform.position - transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (patrolling == 1 && !isDead) {
+            if (Vector3.Distance(transform.position, targets[target_index].transform.position) < 1.0f) {
+                target_index = (target_index + 1) % 4;
+                Debug.Log("*******************************************************************************************************************");
+                Debug.Log(target_index);
+                //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(targets[target_index].transform.position - transform.position), Time.deltaTime);
+                transform.rotation = Quaternion.LookRotation(targets[target_index].transform.position - transform.position);
+            }
+        }
+        
+        //Debug.Log(Vector3.Angle(player.transform.position - transform.position, transform.forward));
+        //Invoke("Reloading", 3.0f);
+
+        // Detect Player code
+        float angle_with_player = Vector3.Angle(player.transform.position - transform.position, transform.forward);
+        float distance_with_player = Vector3.Distance(player.transform.position, transform.position);
+        //Debug.Log(angle_with_player);
+        if ((angle_with_player < Math.Abs(detection_angle)) && !isDead && (distance_with_player < detection_distance)){
+            patrolling = 0;
+            detected_player(distance_with_player);
+        }
+    }
+
+    void detected_player(float distance_with_player){
+        patrolling = 0;
+        transform.rotation = Quaternion.LookRotation(player.transform.position - transform.position);
+        animator.SetTrigger("player_detected");
+        animator.SetTrigger("stop_fire");
+        if ((distance_with_player < lethal_distance)) {
+            animator.SetTrigger("lethal_distance_trigger");
+            animator.SetTrigger("fire");
+
+            // Start shooting the player.
+            RaycastHit hit;
+            int layerMask = 1 << 8;
+            if (gunShotTime >= 0.0f) {
+                gunShotTime -= Time.deltaTime;
+            }
+            // Random vector makes the enemy hit the player with a 20% probability.
+            if (gunShotTime <= 0) {
+                gunShotTime = 0.5f;
+                Vector3 randomVector = new Vector3(UnityEngine.Random.Range(0f, 0f), UnityEngine.Random.Range(-5f, 5f), UnityEngine.Random.Range(-0f, 0f));
+                if(Physics.Raycast(end.transform.position, ((end.transform.position - start.transform.position)+randomVector).normalized, out hit, 100.0f, layerMask)) {
+                    Debug.Log("Enemy Hit player");
+                    player.GetComponent<Gun>().Being_shot();
+                }
+                else {
+                    // Debug.Log("Enemey did not hit player");
+                }
+            }
+            
+        } else {
+            animator.SetTrigger("nonlethal_distance_trigger");
+        }
+    }
+
+    public void Being_shot() // getting hit from enemy
+    {
+        // The player shot the enemy. Reduce the enemy health by 20%.
+        if (isDead == false){
+            health = health - 20.0f;
+        }
+        Debug.Log(health);
+        if ((health <= 0.0f) && !isDead){
+            isDead = true;
+            Debug.Log("Enemy Dead");
+            animator.SetTrigger("die");
+        }
+    }
+}
